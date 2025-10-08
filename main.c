1a554c956e4e4b931844e6de249cb64310c63200
nirmalsasindu40 <nirmalsasindu40@gmail.com>
2025-10-09 00:04:39 +0530

    commit

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include<stdlib.h>
 #include "heder.h"
 
 char choice;
 
 int main(){
+    system("cls");
     printf("==========================================================================================\n");
     printf("WELCOME TO THE PARLIMENT ELECTION SYSTEM\n");
     printf("==========================================================================================\n\n1");
 
     do{
-    
 
     printf("Menu:\n\n");
     printf("1.Adimn panel\n");
@@ -30,36 +31,44 @@ int main(){
 
     switch(choice){
         case '1':
+            system("cls");
             printf("\nyou are in ADMIN PANEL\n");
             break;
 
         case '2':
+            system("cls");
             printf("\nyou are Voter Registration\n");
             voter_registration();
             break;
 
         case '3':
+            system("cls");
             printf("\nyou are in Candidate Registration\n");
             candidate_registration();
             break;
 
         case '4': 
+            system("cls");
             printf("\nyou are in party registertion\n");
             break;
 
         case '5':
+            system("cls");
             printf("\nyou are in polling\n");
             break;
 
         case '6':
+            system("cls");
             printf("\nyou are in results\n\n");
             break;
 
         case '0':
+            system("cls");
             printf("\nyou are choosed exit................\n\n");
             break;
 
         default:
+            system("cls");
             printf("\nenter the corrrect valied number\n");
 
 
