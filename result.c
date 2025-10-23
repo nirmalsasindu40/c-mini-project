9fe9d427b92cba8267211c99d6e1b1ff3255fb3b
sandaru <sandarunimsara096@gmail.com>
2025-10-23 22:40:25 +0530

    Add basic election results display feature

--- result.c
+++ result.c
@@ -1,12 +1,70 @@
 #include<stdio.h>
 #include<string.h>
-#include<stdlib.h>
 #include "heder.h"
-//function for read the candidate registration data file
-int count_all_votes(char names[][100], char parties[][150], char districts[][25], 
-                    int election_numbers[], int vote_counts[], int max_candidates) {
-    FILE *fp_candidates = fopen("candidate registration data.txt", "r");
-    if (fp_candidates == NULL) {
-        printf("No candidates registered!\n");
-        return 0;
+
+// Struct to hold vote data
+struct Vote {
+    char voter_nic[50];
+    char candidate_name[100];
+    char party[150];
+    char district[25];
+};
+
+// Function to display overall results
+void display_overall_results() {
+    FILE *fp;
+    struct Vote vote;
+    char line[500];
+    int total_votes = 0;
+    
+    printf("\n========== OVERALL ELECTION RESULTS ==========\n\n");
+    
+    fp = fopen("votes.txt", "r");
+    
+    if (fp == NULL) {
+        printf("No votes found or file doesn't exist!\n");
+        separator();
+        return;
     }
+    
+    // Count total votes
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        total_votes++;
+    }
+    
+    printf("Total Votes Cast: %d\n\n", total_votes);
+    
+    fclose(fp);
+    separator();
+}
+
+// Main results menu
+void results() {
+    char choice;
+    
+    do {
+        printf("\n========== ELECTION RESULTS MENU ==========\n\n");
+        printf("1. Overall Results\n");
+        printf("0. Back to Main Menu\n\n");
+        separator();
+        
+        printf("Enter your choice: ");
+        scanf(" %c", &choice);
+        
+        switch(choice) {
+            case '1':
+                display_overall_results();
+                break;
+            
+            case '0':
+                printf("\nReturning to main menu...\n");
+                separator();
+                break;
+            
+            default:
+                printf("\nInvalid choice! Please try again.\n");
+                separator();
+        }
+        
+    } while (choice != '0');
+}
