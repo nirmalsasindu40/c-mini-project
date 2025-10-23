11b504a70d14a224c6f28d956cb4d0ae0ad3974f
sandaru <sandarunimsara096@gmail.com>
2025-10-23 15:14:08 +0530

    function for read the candidate registration data file

--- result.c
+++ result.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
-int main(){
-    int a;
-    int b;
-    int c;
-    int s;
-
-return 0;    
-}
+#include<string.h>
+#include<stdlib.h>
+#include "heder.h"
+//function for read the candidate registration data file
+int count_all_votes(char names[][100], char parties[][150], char districts[][25], 
+                    int election_numbers[], int vote_counts[], int max_candidates) {
+    FILE *fp_candidates = fopen("candidate registration data.txt", "r");
+    if (fp_candidates == NULL) {
+        printf("No candidates registered!\n");
+        return 0;
+    }
